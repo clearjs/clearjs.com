fcd0e70415eae3fe6f5e13260c074a7c2803eb79
Roman Boiko <rb@clearjs.com>
2015-07-18 19:45:27 +0300

    Set up linting for JavaScript.

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ import koa from 'koa';
 
 const app = koa();
 
-app.use(function *(next) {
-  const start = new Date;
+app.use(function *logger(next) {
+  const start = new Date();
   yield next;
-  const ms = new Date - start;
+  const ms = new Date() - start;
   console.log('%s %s %s - %s', this.method, this.url, this.ip, ms);
 });
 
-app.use(function *() {
+app.use(function *handler() {
   this.response.redirect(process.env.NEW_BASE_URL);
 });
 

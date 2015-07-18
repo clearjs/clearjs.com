aab517856c4c312ef5e8da237e1120ffdc403c9e
Roman Boiko <rb@clearjs.com>
2015-07-18 16:52:11 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# clearjs
+
+> clearjs.com website implementation
---
+++ app.js
@@ -0,0 +1,17 @@
+import koa from 'koa';
+
+const app = koa();
+
+app.use(function *(next) {
+  const start = new Date;
+  yield next;
+  const ms = new Date - start;
+  console.log('%s %s %s - %s', this.method, this.url, this.ip, ms);
+});
+
+app.use(function *() {
+  this.response.redirect(process.env.NEW_BASE_URL);
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
